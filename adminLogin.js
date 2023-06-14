cc1b46c8246438cf7dd8163525829552db89a417
Shubham <122727148+i-shubham-jha@users.noreply.github.com>
2023-06-14 10:49:22 +0530

    Update adminLogin.js

--- adminLogin.js
+++ adminLogin.js
@@ -5,12 +5,7 @@ import { getAuth, createUserWithEmailAndPassword, signOut,
 } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUfqyLzc9d9WOSwXVdqhJoUMSCcCTV9xM",
-  authDomain: "hostel-management-system-70942.firebaseapp.com",
-  projectId: "hostel-management-system-70942",
-  storageBucket: "hostel-management-system-70942.appspot.com",
-  messagingSenderId: "552250191673",
-  appId: "1:552250191673:web:607cf0a580d375bab182e8"
+  // enter your own firbase config and API keys here
 };
 
 const app = initializeApp(firebaseConfig);

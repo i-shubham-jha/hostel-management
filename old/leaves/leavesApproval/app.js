39c31d6adc6f99e86e1523ff01709fa1d1c0f8c4
Shubham <122727148+i-shubham-jha@users.noreply.github.com>
2023-06-14 10:55:32 +0530

    Delete old/leaves directory

--- old/leaves/leavesApproval/app.js
+++
@@ -1,4 +0,0 @@
-// trying to get the id in the url
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get('id')// this id contains the id passed in the url

be158a7d32405b57f2ae70e9570c3f27ae6bb8a7
Shubham <122727148+i-shubham-jha@users.noreply.github.com>
2023-06-14 10:53:44 +0530

    Update pendingComplaints.js

--- student/complaints/pendingComplaints.js
+++ student/complaints/pendingComplaints.js
@@ -5,12 +5,7 @@ import { getAuth, createUserWithEmailAndPassword, signOut,
 import { getFirestore, doc, getDoc, getDocs, collection, addDoc, deleteDoc, onSnapshot, query, where, setDoc, updateDoc, getCountFromServer} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUfqyLzc9d9WOSwXVdqhJoUMSCcCTV9xM",
-  authDomain: "hostel-management-system-70942.firebaseapp.com",
-  projectId: "hostel-management-system-70942",
-  storageBucket: "hostel-management-system-70942.appspot.com",
-  messagingSenderId: "552250191673",
-  appId: "1:552250191673:web:607cf0a580d375bab182e8"
+  // enter your own firbase config and API keys here
 };
 
 const app = initializeApp(firebaseConfig);
